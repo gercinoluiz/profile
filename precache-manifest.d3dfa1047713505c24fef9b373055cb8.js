fbcf0458894b9c2ce425989ba1df3d5f9add478e
Gercino Luiz <gercino.neto.luiz@gmail.com>
2020-10-27 12:19:50 -0300

    Updates

---
+++ precache-manifest.d3dfa1047713505c24fef9b373055cb8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43ae471aab4c91d4c8dde5398c5ae317",
+    "url": "/gercinoluiz/profile/index.html"
+  },
+  {
+    "revision": "7cec3687188a7faeb6fc",
+    "url": "/gercinoluiz/profile/static/css/main.094f5acf.chunk.css"
+  },
+  {
+    "revision": "b04f219df9d30d9c19c9",
+    "url": "/gercinoluiz/profile/static/js/2.e26803b5.chunk.js"
+  },
+  {
+    "revision": "0dc713a05373e5c9a88cb999f31ae6e4",
+    "url": "/gercinoluiz/profile/static/js/2.e26803b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cec3687188a7faeb6fc",
+    "url": "/gercinoluiz/profile/static/js/main.05fa7d34.chunk.js"
+  },
+  {
+    "revision": "cda02701d53f8f7c4acc",
+    "url": "/gercinoluiz/profile/static/js/runtime-main.fd5bae9f.js"
+  },
+  {
+    "revision": "84886676a792c1ac8175ca15ffac3832",
+    "url": "/gercinoluiz/profile/static/media/Gercino Luiz.84886676.pdf"
+  },
+  {
+    "revision": "7640b20068f55d0a9476afb566b19c00",
+    "url": "/gercinoluiz/profile/static/media/myServices.7640b200.png"
+  },
+  {
+    "revision": "640fcb54cde14ccf0873341efa56cd6e",
+    "url": "/gercinoluiz/profile/static/media/orphanages.640fcb54.png"
+  },
+  {
+    "revision": "6d92f3b5deed3d55035e4dd8a15164ec",
+    "url": "/gercinoluiz/profile/static/media/profile.6d92f3b5.png"
+  }
+]);
